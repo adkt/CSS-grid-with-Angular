eb5bc22d130d8f03ca7e07d816ebd67c142fda1e
adkt <adkt@outlook.com>
2019-03-21 10:26:20 +1100

    3x2 Grid now created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CMyGridComponent } from './c-my-grid/c-my-grid.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CMyGridComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/c-my-grid/c-my-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-my-grid',
+  templateUrl: './c-my-grid.component.html',
+  styleUrls: ['./c-my-grid.component.css']
+})
+export class CMyGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
